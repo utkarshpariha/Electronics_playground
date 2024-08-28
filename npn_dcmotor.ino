51359998b016f1325d53b80059e9942f2cd77a9b
Utkarsh Singh Parihar <103374960+utkarshpariha@users.noreply.github.com>
2024-08-28 19:26:12 +0530

    Update npn_dcmotor.ino

--- npn_dcmotor.ino
+++ npn_dcmotor.ino
@@ -1,5 +1,5 @@
-
-int potPin=0;
+// initialize potentiometer pin as 0 and potentiometer & motorspeed value as 0 
+int potPin=0; 
 int motorPin= 5;
 int potVal=0;
 int motorSpeed=0;
@@ -15,6 +15,10 @@ void loop(){
   Serial.print("potvalue");
   Serial.println(potVal);
   delay(2000);
+ //Initialize the motor speed and pot value before 
+ // who is going to be mapped=(from which mapping take place,min value,max value,min,max)
+ // analogRead has 10 bit ADC, potentiometer 0 to 1023
+ // motor will run on 0 to 255 as PWM Signal controls it
  motorSpeed = map(potVal,0,1023,0,255);
   Serial.print("motorSpeed");
   Serial.println(motorSpeed);
